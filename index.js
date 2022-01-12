e70fb7ac24caef1ee6fa5215a7ec690d2c96d6dd
John Mook <jedwardmook@yahoo.com>
2022-01-12 11:03:25 -0600

    commit tree 7882d7bf0742ad61e34d9e1624ee503ba543a750

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function fareMultiplier() {
+    return (5*5);
+}
+
+const createFareMultiplier = function(){
+    return fareMultiplier;
+}
+
+const fareDoubler = fare => fare*2;
+
+const fareTripler = fare => fare*3;
+
+function selectDifferentDrivers (differentDrivers, whichTwo){
+    return whichTwo(differentDrivers);
+}
